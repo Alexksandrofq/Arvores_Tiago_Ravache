a66751247980f72d7f31d04678416334f71872b8
Alexksandro Fq <alefernandesdequeiroz@gmail.com>
2025-05-15 21:02:23 -0300

    implementacao

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        class No{
+            public int conteudo;
+            public No filhoEsquerdo;
+            public No filhoDireito;
+        }
+
+        class Arvores{
+            public No raiz;
+        }
+
+    }
+}
